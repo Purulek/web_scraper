af8201df50810a6db89330d4c41da49f35a26b65
purulek <adam.purul@gmail.com>
2024-11-03 23:49:29 +0100

    next commit 01.11

--- web_scraper.py
+++ web_scraper.py
@@ -10,4 +10,14 @@ import csv
 with open ("urls_to_scrap",'r',encoding= 'utf-8') as file:
     for url in file.readline():
         url = url.strip()
+
         pass
+
+
+def open_website(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    products = soup.find_all('div', class_=" title=")
+    print (products)
+
+open_website("XXX")
