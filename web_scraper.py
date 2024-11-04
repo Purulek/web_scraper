22763e8a2ea7cadf5d66643e47e945d0de6805ee
purulek <adam.purul@gmail.com>
2024-11-04 23:29:16 +0100

    next commit 01.11

--- web_scraper.py
+++ web_scraper.py
@@ -17,7 +17,12 @@ with open ("urls_to_scrap",'r',encoding= 'utf-8') as file:
 def open_website(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    products = soup.find_all('div', class_=" title=")
-    print (products)
+    products = soup.find('strong', class_="projector_prices__price")
+    price = products.text.strip()
+    print("price: {}".format(price))
 
+
+
+
+    
 open_website("XXX")
