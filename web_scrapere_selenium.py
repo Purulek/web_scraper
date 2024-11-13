feff5563dd9faa09fc0526f54dbc2fe8aca0c949
purulek <adam.purul@gmail.com>
2024-11-13 23:55:03 +0100

    next ommit 13.11

--- web_scrapere_selenium.py
+++ web_scrapere_selenium.py
@@ -1,19 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.edge.options import Options
 import requests
 
 
-driver = webdriver.Edge()
 
+options = Options()
+#options.add_argument("--headless")
+options.add_argument("--inprivate")  
+driver = webdriver.Edge(options=options)
 
+driver.get("https://strefamtg.pl")
+search_box = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, "s"))
+    )
 
-driver.get("https://flamberg.com.pl/pl/menu/warhammer-40000-268")
-search_box = driver.find_element(By.NAME, "q")
 search_box.send_keys("Thousand Sons Magnus the Red")
-print(driver.current_url)
 search_box.send_keys(Keys.RETURN)
-print(driver.current_url)
+"""WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CLASS_NAME, ".product-quantities__val--success"))  
+)"""
+first_product = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "product-quantities product-quantities--smaller mb-2 "))
+    )
+first_product.click()
 
 
 
