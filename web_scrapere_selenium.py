73ee98dd2b7b7ecd6039b6d4c2870b1adfe21989
purulek <adam.purul@gmail.com>
2024-11-18 23:56:14 +0100

    next commit 18.11

--- web_scrapere_selenium.py
+++ web_scrapere_selenium.py
@@ -4,34 +4,67 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.edge.options import Options
+import os
+
+urls = ["https://flamberg.com.pl", 'https://strefamtg.pl']
 
 
 
-product = input("Which item do you want to compare prices for?")
-options = Options()
-#options.add_argument("--headless")
-options.add_argument("--inprivate")  
-driver = webdriver.Edge(options=options)
+def get_url (urls):
+    # defnitons 
+    product = input("Which item do you want to compare prices for?")
+    options = Options()
 
-driver.get("https://flamberg.com.pl/")
-search_box = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//input[@name='text' and @placeholder='Wpisz czego szukasz']"))
-    )
+    #web- settings
+    options.add_argument("--headless")
+    options.add_argument("--inprivate")  
+    driver = webdriver.Edge(options=options)
+
+    #web- functions 
+    for url in urls:
+
+        driver.get(url)
+        if url == "https://flamberg.com.pl":
+            search_box = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//input[@name='text' and @placeholder='Wpisz czego szukasz']"))
+                )
+            search_box.send_keys(product)
+            search_box.send_keys(Keys.RETURN)
+            current_url = driver.current_url 
+
+            #list- appending url to file
+            with open ("urls_to_scrap", "a",encoding= 'utf-8') as f:
+                f.write(current_url)
+        elif url == 'https://strefamtg.pl':
+            search_box = WebDriverWait(driver, 10).until(
+                        EC.presence_of_element_located((By.NAME, "s")))
+            search_box.send_keys(product)
+            search_box.send_keys(Keys.RETURN)
+            current_url = driver.current_url 
+
+            #list- appending url to file
+            with open ("urls_to_scrap", "a",encoding= 'utf-8') as f:
+                f.write(current_url)
+
+
+
+get_url(urls)
 
 
 
 
-search_box.send_keys(product)
-search_box.send_keys(Keys.RETURN)
-
-
-current_url = driver.current_url
-
-
-
-with open ("urls_to_scrap", "w+",encoding= 'utf-8') as f:
-    f.write(current_url)
 
 
 
 
+
+
+
+
+
+
+
+
+"""search_box = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, "s"))
+    )"""
